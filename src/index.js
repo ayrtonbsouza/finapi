4e7e1db1390b1e8b96bfcef483686bde02eee751
Ayrton Souza <ayrtonsouza@icloud.com>
2021-08-23 17:37:18 -0300

    [:construction:] initial account post route development

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
-const app = express()
+const app = express();
+app.use(express.json())
+const customers = []
+
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body;
+  const id = uuidv4();
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  })
+  return response.status(201).send()
+})
 
 app.listen(3333, () => {
   console.log('🚀 Server started and running in: http://localhost:3333/')
