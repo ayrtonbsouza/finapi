abdc7c4521a0242bc29388943793ba2d7f29f774
Ayrton Souza <ayrtonsouza@icloud.com>
2021-08-23 22:25:54 -0300

    [:construction:] developing error fallback for user not existent

--- src/index.js
+++ src/index.js
@@ -23,6 +23,9 @@ app.post('/account', (request, response) => {
 app.get('/statement/:cpf', (request, response) => {
   const { cpf } = request.params;
   const customer = customers.find(customer => customer.cpf === cpf)
+  if (!customer) {
+    return response.status(400).json({ error: 'Customer not found.' })
+  }
   return response.status(200).json(customer.statement);
 })
 
