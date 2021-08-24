0c4230aaf66c7b923ef84a41133388e363cc3790
Ayrton Souza <ayrtonsouza@icloud.com>
2021-08-23 22:34:42 -0300

    [:hammer:] turning the user verification into a middleware

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ const app = express();
 app.use(express.json())
 const customers = []
 
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+  const customer = customers.find(customer => customer.cpf === cpf)
+  if (!customer) {
+    return response.status(400).json({ error: 'Customer not found.' })
+  }
+  request.customer = customer;
+  return next();
+}
+
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
   const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
@@ -20,12 +30,8 @@ app.post('/account', (request, response) => {
   return response.status(201).send()
 })
 
-app.get('/statement', (request, response) => {
-  const { cpf } = request.headers;
-  const customer = customers.find(customer => customer.cpf === cpf)
-  if (!customer) {
-    return response.status(400).json({ error: 'Customer not found.' })
-  }
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
   return response.status(200).json(customer.statement);
 })
 
